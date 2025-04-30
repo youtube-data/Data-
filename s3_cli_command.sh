acc3145f135072ee1bf846589e0ed9196d1fae7d
Binaya <binaydhakal35@gmail.com>
2025-04-30 11:01:06 -0400

    added the aws code of ETL

---
+++ s3_cli_command.sh
@@ -0,0 +1,17 @@
+#Replace It With Your Bucket Name
+bucket_name=binay-youtube-raw-useast1-dev
+
+aws s3 cp . s3://{bucket_name}/raw_statistics_reference_data/ --recursive --exclude "*" --include "*.json"
+
+aws s3 cp CAvideos.csv s3://{bucket_name}/raw_statistics/region=ca/
+aws s3 cp DEvideos.csv s3://{bucket_name}/raw_statistics/region=de/
+aws s3 cp FRvideos.csv s3://{bucket_name}/raw_statistics/region=fr/
+aws s3 cp GBvideos.csv s3://{bucket_name}/raw_statistics/region=gb/
+aws s3 cp INvideos.csv s3://{bucket_name}/raw_statistics/region=in/
+aws s3 cp JPvideos.csv s3://{bucket_name}/raw_statistics/region=jp/
+aws s3 cp KRvideos.csv s3://{bucket_name}/raw_statistics/region=kr/
+aws s3 cp MXvideos.csv s3://{bucket_name}/raw_statistics/region=mx/
+aws s3 cp RUvideos.csv s3://{bucket_name}/raw_statistics/region=ru/
+aws s3 cp USvideos.csv s3://{bucket_name}/raw_statistics/region=us/
+
+
